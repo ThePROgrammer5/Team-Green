0fca285c926473dacba47b84e8e0cfb23d60c510
The PROgrammer / <codingplusgaming@gmail.com>
2022-05-17 11:30:58 -0700

    done

--- custom seed order/custom.js
+++ custom seed order/custom.js
@@ -3,3 +3,13 @@ var order = {
     nSeed: 0,
     pSeed: 0,
 };
+
+function addToKart(item, no) {
+    kart.item = kart.item + int(no);
+}
+
+function order() {
+    var email = document.createElement("a");
+    email.href = `mailto=codingplusgaming@gmail.com&subject=Team Green Order&body=${order}`;
+    email.click();
+}
